01be8a586cd3ba05c464a4d726d6345e36afe07c
Walid <boulazzafirwalid@gmail.com>
2025-06-30 03:28:21 +0100

    fixing the data error

--- html/php/data.php
+++ html/php/data.php
@@ -1,18 +1,18 @@
 <?php
     while ($row = mysqli_fetch_assoc($sql)) {
         $sql2 =  "SELECT * FROM messages WHERE (incoming_msg_id = {$row['unique_id']}
-                  OR outgoing_msg_id = {$row['unique_id']}) AND (outgoing_msg_id = {$outgoing_id})
-                  OR (outgoing_msg_id = {$outgoing_id}) ORDER BY msg_id DESC LIMIT 1";
+                  OR outgoing_msg_id = {$row['unique_id']}) AND (outgoing_msg_id = {$outgoing_id}
+                  OR incoming_msg_id = {$outgoing_id}) ORDER BY msg_id DESC LIMIT 1";
         $query2 = mysqli_query($conn, $sql2);
         $row2 = mysqli_fetch_assoc($query2);
-        if( mysqli_num_rows($query2) > 0) {
+        if ($row2 && mysqli_num_rows($query2) > 0) {
             $result = $row2['msg'];
         } else {
             $result = "No messages available";
         }
 
         (strlen($result) > 28) ? $msg = substr($result, 0, 28). "..." : $msg = $result;
-        ($outgoing_id == $row2['outgoing_msg_id']) ? $you = "You: " : $you = "";
+        $you = ($row2 && $outgoing_id == $row2['outgoing_msg_id']) ? "You: " : "";
         ($row['status'] == "Offline now") ? $offline = "offline" : $offline = "";
 
         $output .= '<a href="chat.php?user_id=' . $row['unique_id'] . '">
